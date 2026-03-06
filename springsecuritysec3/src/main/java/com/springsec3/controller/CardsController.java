c41776a4408641631929cf73e18db727b081e4b3
Rishabh Sharma <sharma.rishabh@myyahoo.com>
2026-03-06 09:14:04 +0530

    initial commit section3

--- springsecuritysec3/src/main/java/com/springsec3/controller/CardsController.java
+++ springsecuritysec3/src/main/java/com/springsec3/controller/CardsController.java
@@ -1,4 +1,4 @@
-package com.springsec2.controller;
+package com.springsec3.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- springsecuritysec3/src/main/java/com/springsec3/controller/LoansController.java
+++ springsecuritysec3/src/main/java/com/springsec3/controller/LoansController.java
@@ -1,4 +1,4 @@
-package com.springsec2.controller;
+package com.springsec3.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
