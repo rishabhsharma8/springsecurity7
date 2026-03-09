02107e9e0dd10c0c9013dc337829ace320ec9783
Rishabh Sharma <sharma.rishabh@myyahoo.com>
2026-03-09 16:24:51 +0530

    initial commit section3

--- springsecuritysec3/src/main/java/com/springsec3/config/SecurityConfig.java
+++ springsecuritysec3/src/main/java/com/springsec3/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.springsec3.config;
+package com.springsec2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .build();
 
         UserDetails normalUser = User.withUsername("mohit")
-                .password("{bcrypt}$2a$12$2acCjdotaMutD6R4kf9xfevlrFWsCkW9jaWRHP2urwFW2KOKqgUY2")
+                .password("{bcrypt}$2a$12$2acCjdotaMutD6R4kf9xfevlrFWsCkW9jaWRHP2urwFW2KOKqgUY2")  //mohit
                 .authorities("read")
                 .build();
 
