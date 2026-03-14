45339e191721d727252a93ef2d559bdb0466d1b0
Rishabh Sharma <sharma.rishabh@myyahoo.com>
2026-03-14 22:21:17 +0530

    Session#15

--- springsecuritysec3/src/main/java/com/springsec3/config/SecurityConfig.java
+++ springsecuritysec3/src/main/java/com/springsec3/config/SecurityConfig.java
@@ -5,17 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -23,9 +16,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain customSecurityFilterChain(HttpSecurity http) {
         http.authorizeHttpRequests(r -> r.requestMatchers("/balance/**","/cards/**","/loans/api/v1/details","/accounts/**").authenticated()
-                .requestMatchers("/contacts/**","/notices/**").permitAll());
+                .requestMatchers("/contacts/**","/notices/**","/customer/api/v1/register").permitAll());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
         return http.build();
     }
 
